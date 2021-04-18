01e306c2e9c1e0f53e33bc2f1887c095d5dfd719
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2021-04-17 22:47:39 -0700

    add convar check
    make functions respect the spd_tool_toggle_adminonly convar

--- lua/entities/gmod_wire_expression2/core/custom/simple_prop_damage.lua
+++ lua/entities/gmod_wire_expression2/core/custom/simple_prop_damage.lua
@@ -2,14 +2,20 @@ E2Lib.RegisterExtension( "simple_prop_damage", true )
 
 e2function void entity:spdAdminDisable()
     if not isValid( this ) then return end
-    if not self.player:IsAdmin() then return end
+    
+    if GetConVar( "spd_tool_toggle_adminonly" ):GetBool() then
+        if not self.player:IsAdmin() then return end
+    end
     
     this.spdDisabled = 1
 end
 
 e2function void entity:spdAdminEnable()
     if not isValid( this ) then return end
-    if not self.player:IsAdmin() then return end
+    
+    if GetConVar( "spd_tool_toggle_adminonly" ):GetBool() then
+        if not self.player:IsAdmin() then return end
+    end
     
     this.spdDisabled = 0
 end
